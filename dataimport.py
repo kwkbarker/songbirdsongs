ee7e90aa468e9d226ad66b51f6c7e46ebe444a1b
Kevin Barker <kwkbarker@gmail.com>
2021-08-04 00:22:51 -0400

    updates to code data import - still errors

--- dataimport.py
+++ dataimport.py
@@ -10,52 +10,50 @@ from io import BytesIO
 
 def load_data(directory):
     # loads directory
-    # assumes directory contains sound files of bird songs organized into folders by bird type (category)
-    # returns tuple ('sounds', 'label'). audio should be segmented and edited.
-    images = []
-    labels = []
+
     fail = False
+    errors = 0
     for (root,dirs,files) in os.walk(directory, topdown=True):
         # for dir in dirs:
         #     print(dir)
         #     os.chdir(os.path.join(root, dir))
             # for (root,dirs,files) in os.walk(os.path.join(root, dir), topdown=True):
-        print(files)
-    #     for file in files:
-    #         path = os.path.join(root, file)
-    #         print(path)
-    #         canvas = audio_to_image(path)
+        # print(files)
+        for file in files:
+            path = os.path.join(root, file)
+            print(path)
+            canvas = audio_to_image(path)
             
-    #         # img = cv2.imread(os.path.join(root, filename_img), cv2.IMREAD_UNCHANGED)
-    #         if canvas is not None:
+            # img = cv2.imread(os.path.join(root, filename_img), cv2.IMREAD_UNCHANGED)
+            if canvas is not None:
                 
-    #             # file_img = cv2.imread(png, cv2.IMREAD_UNCHANGED)
-    #             # cv2.imshow('image', file_img)
-    #             # cv2.waitKey(0)
-    #             # resized = cv2.resize(file_img, (30, 30))
-    #             save_path = os.path.join(os.path.split(root)[1])                
-    #             filename = f"{file}.png"
-    #             try:
-    #                 os.chdir('new_dataset')
-    #             except FileNotFoundError:
-    #                 os.mkdir('new_dataset')
-    #                 os.chdir('new_dataset')
-    #             # cv2.imwrite(filename, resized)
-    #             if save_path:
-    #                 try:
-    #                     os.chdir(save_path)
-    #                 except FileNotFoundError:
-    #                     os.mkdir(save_path)
-    #                     os.chdir(save_path)
-    #             png_output = filename
-    #             canvas.print_png(png_output)
-    #         else:
-    #             print("No image returned.")
-    #             fail = True
-    # if fail == False:
-    #     print("Data successfully loaded.")
-    # else:
-    #     print("Something went wrong.")
+                # file_img = cv2.imread(png, cv2.IMREAD_UNCHANGED)
+                # cv2.imshow('image', file_img)
+                # cv2.waitKey(0)
+                # resized = cv2.resize(file_img, (30, 30))
+                save_path = os.path.join(os.path.split(root)[1])                
+                filename = f"{file}.png"
+                try:
+                    os.chdir('new_dataset')
+                except FileNotFoundError:
+                    os.mkdir('new_dataset')
+                    os.chdir('new_dataset')
+                if save_path:
+                    try:
+                        os.chdir(save_path)
+                    except FileNotFoundError:
+                        os.mkdir(save_path)
+                        os.chdir(save_path)
+                png_output = filename
+                canvas.print_png(png_output)
+            else:
+                print("No image returned.")
+                fail = True
+                errors += 1
+    if fail == False:
+        print("Data successfully loaded.")
+    else:
+        print(f"Something went wrong. There were {errors} errors.")
 
 def audio_to_image(file):
     # converts audio file into spectrogram image.
@@ -70,7 +68,7 @@ def audio_to_image(file):
         # png_output = 'test.png'
         # image = canvas.print_png(png_output)
     except FileNotFoundError:
-        print('File not found.')
+        print(f'File not found: {file}')
         return None
     return canvas
 
